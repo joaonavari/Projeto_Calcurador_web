7fdb0573250c3d39333a246c03def10313e8b37f
Joao Vitor Navari <joaovitorne.16@gmail.com>
2026-02-25 21:53:46 -0300

    Calculadora js

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+function criaCalculadora (){
+    return{
+        display: document.querySelector('.display'),
+        inicia() {
+            this.clickbotoes();
+            this.precionaEnter();
+        },
+        
+        precionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.key === 'Enter'){
+                    this.realizaConta();
+                }
+            });
+        },
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+            
+            try {
+                conta = eval(conta);
+                if(!conta){ 
+                    alert(`conta inválida`);
+                    return;
+                }
+
+                this.display.value = String(conta); 
+            } catch (e) {
+                alert(`conta inválida`);
+                return;
+            }
+        },
+
+        clickbotoes(){
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+            this.display.focus();
+        }
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
